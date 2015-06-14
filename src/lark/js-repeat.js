1f129956b7049f8713e2c901ed7cb1deada65815
jaceyshome <jaceyshome@gmail.com>
2015-06-14 19:55:37 +1000

    update js repeat

--- src/lark/js-repeat.js
+++ src/lark/js-repeat.js
@@ -3,53 +3,28 @@ lark.addComponent('jsRepeat',[function(){
     return {
       priority: 1000,
       link: (function($scope,$element,$attr){
-        var expression = $element.getAttribute('js-repeat') || $element.getAttribute('data-js-repeat'), results, match, targetObjKey, parentObjKey, childElement, scope,
-          firstChild = true, totalChildren = 0,
-          startMarker = "jsRepeat: {{match}} start",
-          endMarker = "jsRepeat: {{match}} end"
-          ;
-        if(expression){
-          results = expression.match(/(\w+)\s+?[Ii][Nn]\s+?(\w+)/);
-          match = results[0];
-          targetObjKey = results[1];
-          parentObjKey = results[2];
-          if(typeof $scope.$$parent[parentObjKey] != undefined && $scope[targetObjKey] == undefined){
-            for(var key in $scope.$$parent[parentObjKey]){
-              if($scope.$$parent[parentObjKey].hasOwnProperty(key)){
-                if(firstChild == true){
-                  setFirstChild($scope.$$parent[parentObjKey][key], targetObjKey);
-                  firstChild = false;
-                }else{
-                  childElement = $element.cloneNode(true);
-                  $scope.$$parent.$$element.appendChild(childElement);
-                  scope = lark.createScope($scope.$$parent,childElement);
-                  scope[targetObjKey] = $scope.$$parent[parentObjKey][key];
-                  lark.bindComponentsToScope(scope, childElement);
-                  addCommentBeforeChild(endMarker, childElement);
-                  addCommentBeforeChild(startMarker, childElement);
-                }
-              }
+        var expression = $element.getAttribute('js-repeat') || $element.getAttribute('data-js-repeat'), element, scope, scopeObj ,
+          results = expression.match(/(\w+)\s+?[Ii][Nn]\s+?(\w+)/),
+          match = results[0],
+          targetKey = results[1],
+          scopeObjKey = results[2];
+
+        if(expression && $scope[targetKey] == undefined){
+          scopeObj = $scope.$getExpressionValue(scopeObjKey);
+          (typeof scopeObj != undefined) && Array.prototype.forEach.call(scopeObj, function(obj, index, array){
+            if(index == 0){
+              $scope[targetKey] = obj;
+            }else{
+              //create new scope and element
+              scope = lark.createScope($scope.$$parent);
+              scope.extend($scope);
+              scope[targetKey] = obj;
+              element = jQuery.clone($element);
+              $element.parentElement.appendChild(element);
+              lark.bindMatchedComponents(scope,element);
             }
-            childElement = null;
-            $scope.$$parent.$$element.appendChild(
-              document.createComment(endMarker.replace(/{{match}}/, match))
-            );
-          }
+          });
         }
-
-        function setFirstChild(object, key){
-          $scope[key] = object;
-          addCommentBeforeChild(startMarker, $scope.$$parent.$$element.firstChild);
-        }
-
-        function addCommentBeforeChild(marker, childNode){
-          $scope.$$parent.$$element.insertBefore(
-            document.createComment(marker.replace(/{{match}}/, match)),
-            childNode
-          );
-        }
-
-
       })
     }
   }
