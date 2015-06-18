a83b641e189199ab2a20d76869cbd1fc6aa03193
Jacob Wang <jacob.wang@change2.com.au>
2015-06-18 18:11:06 +1000

    update the value only if the old value if the same as new value

--- src/lark/js-src.js
+++ src/lark/js-src.js
@@ -2,15 +2,18 @@ lark.addComponent('jsSrc',[function(){
   return function(){
     return {
       link: (function($scope,$element,$attr){
+        var oldValue = null;
         $scope.$watch(
           function(){
             return $element.getAttribute('js-src') || $element.getAttribute('data-js-src');
           },
-          function(val){
-            if(val){
-              $element.setAttribute('src',val);
+          function(newValue){
+            if(oldValue != newValue){
+              $element.setAttribute('src',newValue);
+              oldValue = newValue;
             }
-        });
+          }
+        );
       })
     }
   }
