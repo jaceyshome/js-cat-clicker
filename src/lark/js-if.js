0c49a675de8a63277a3e1d8b03f289a14c5ed9e6
jaceyshome <jaceyshome@gmail.com>
2015-06-19 13:23:46 +1000

    update js-if

--- src/lark/js-if.js
+++ src/lark/js-if.js
@@ -5,10 +5,8 @@ lark.addComponent('jsIf',[function(){
         var expression = $element.getAttribute('js-if') || $element.getAttribute('data-js-if');
         var $parentNode = $element.parentNode;
         $scope.$watch(expression,function(val){
-          if(val){
-            if(!$element.parentNode){
-              $parentNode.appendChild($element);
-            }
+          if(val && !$element.parentNode != $parentNode){
+            $parentNode.appendChild($element);
           }else{
             $parentNode.removeChild($element);
           }
